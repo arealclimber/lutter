14a2c8577055540708041efa84c6161eaa01be28
arealclimber <luffulgence@gmail.com>
2022-08-10 16:31:04 +0800

    Image URL Functionality 🥰

--- components/Tweetbox.tsx
+++ components/Tweetbox.tsx
@@ -5,12 +5,26 @@ import {
 	PhotographIcon,
 	SearchCircleIcon,
 } from '@heroicons/react/outline';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useSession } from 'next-auth/react';
 
 const TweetBox = () => {
 	const [input, setInput] = useState<string>('');
+	const [image, setImage] = useState<string>('');
 	const { data: session } = useSession();
+	const [imageUrlBoxIsOpen, setImageUrlBoxIsOpen] = useState<boolean>(false);
+
+	const imageInputRef = useRef<HTMLInputElement>(null);
+
+	const addImageToTweet = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+		e.preventDefault();
+
+		if (!imageInputRef.current?.value) return;
+
+		setImage(imageInputRef.current.value);
+		imageInputRef.current.value = '';
+		setImageUrlBoxIsOpen(false);
+	};
 
 	return (
 		<div className="flex space-x-2 p-5">
@@ -36,7 +50,12 @@ const TweetBox = () => {
 					<div className="flex items-center">
 						{/* Utility icons */}
 						<div className="flex flex-1 space-x-2 text-cuteBlue">
-							<PhotographIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+							<PhotographIcon
+								onClick={() =>
+									setImageUrlBoxIsOpen(!imageUrlBoxIsOpen)
+								}
+								className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150"
+							/>
 							<SearchCircleIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
 							<EmojiHappyIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
 							<CalendarIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
@@ -51,6 +70,32 @@ const TweetBox = () => {
 							Tweet
 						</button>
 					</div>
+
+					{imageUrlBoxIsOpen && (
+						<form className="mt-5 flex rounded-lg bg-cuteBlue/80 py-2 px-4">
+							<input
+								ref={imageInputRef}
+								className="flex-1 bg-transparent p-2 text-white outline-none placeholder:text-white"
+								type="text"
+								placeholder="Enter Image URL..."
+							/>
+							<button
+								type="submit"
+								onClick={addImageToTweet}
+								className="font-bold text-white"
+							>
+								Add Image
+							</button>
+						</form>
+					)}
+
+					{image && (
+						<img
+							className="mt-10 h-40 w-full rounded-xl object-contain shadow-lg"
+							src={image}
+							alt=""
+						/>
+					)}
 				</form>
 			</div>
 		</div>
