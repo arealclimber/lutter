efc821ca7003697ad4925504abb4911fa69c4aba
arealclimber <luffulgence@gmail.com>
2022-08-12 12:28:42 +0800

    Web deploy: hide BASE_URL in fetch tweet...etc

--- utils/fetchTweets.ts
+++ utils/fetchTweets.ts
@@ -2,8 +2,9 @@ import { Tweet } from '../typings';
 import { server } from '../config/server';
 
 export const fetchTweets = async () => {
-	const res = await fetch(`${server}/api/getTweets`);
-	// const res = await fetch(`${process.env.BASE_URL}/api/getTweets`);
+	// const res = await fetch(`${server}/api/getTweets`);
+	// ${process.env.BASE_URL}
+	const res = await fetch(`/api/getTweets`);
 
 	const data = await res.json();
 	const tweets: Tweet[] = data.tweets;
