109f0db82adea80272bdd0fad48b49195ed840c0
arealclimber <luffulgence@gmail.com>
2022-08-14 23:31:23 +0800

    to-do

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,43 +9,43 @@ import { fetchTweets } from '../utils/fetchTweets';
 import { Toaster } from 'react-hot-toast';
 
 interface Props {
-	tweets: Tweet[];
+  tweets: Tweet[];
 }
 
 const Home = ({ tweets }: Props) => {
-	// console.log('Home rendering: ');
-	// console.log(tweets);
-	return (
-		<>
-			<div className="lg:max-w-6xl mx-auto max-h-screen overflow-hidden">
-				<Head>
-					<title>Lutter - Twitter Clone</title>
-					<link rel="icon" href="/favicon.ico" />
-				</Head>
+  // console.log('Home rendering: ');
+  // console.log(tweets);
+  return (
+    <>
+      <div className="lg:max-w-6xl mx-auto max-h-screen overflow-hidden">
+        <Head>
+          <title>Lutter - Twitter Clone</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-				<Toaster />
+        <Toaster />
 
-				<main className="grid grid-cols-9">
-					<Sidebar />
-					<Feed tweets={tweets} />
+        <main className="grid grid-cols-9">
+          <Sidebar />
+          <Feed tweets={tweets} />
 
-					<Widgets />
-				</main>
-			</div>
-		</>
-	);
+          <Widgets />
+        </main>
+      </div>
+    </>
+  );
 };
 
 export default Home;
 
-// TODO: WTF is `context`
+// TODO: What on earth is `context`
 export const getServerSideProps: GetServerSideProps = async (context) => {
-	// console.log('getServerSideProps');
-	const tweets = await fetchTweets();
-	// console.log(tweets);
-	return {
-		props: {
-			tweets,
-		},
-	};
+  // console.log('getServerSideProps');
+  const tweets = await fetchTweets();
+  // console.log(tweets);
+  return {
+    props: {
+      tweets,
+    },
+  };
 };
